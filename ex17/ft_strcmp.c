441460e3822128800229d3c6b5aeb260484ea8ca
Gabriel Cesar Nunes Pezoa <gcesar-n@c2r6p3.42sp.org.br>
2024-10-06 14:04:06 -0300

    ex17

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 17:52:13 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/06 13:46:20 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/06 14:03:13 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	//aaaaaaa
-	//dfasdsadsa
-}
+	int	i;
 
+	i = 0;
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+/*
 int	main()
 {
-	char string1[] = "aaa"
-	
-}
+	char string1[] = "aaa";
+	char string2[] = "aaa";
+	printf("%d", ft_strcmp(string1, string2));
+	return (0);
+}*/
