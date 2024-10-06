a68ced7e4ae62a1484a1acefe84958d52283c193
Gabriel Cesar Nunes Pezoa <gcesar-n@c2r6p3.42sp.org.br>
2024-10-06 13:24:41 -0300

    teste git3

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 17:52:13 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/04 19:37:28 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/04 17:52:15 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,11 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	while ((s1[i] != '\0') || (s2[i] != '\0'))
-	{
-		if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] < s2[i])
-			return (-1);
-		i++;
-	}
-	return (0);
+	//aaaaaaa
 }
-/*
+
 int	main()
 {
-	char string1[] = "a";
-	char string2[] = "b";
-	printf("%d", ft_strcmp(string1, string2));
-	return (0);
-}*/
+	char string1[] = "aaa"
+	
+}
