12427924c5aef6eda01b5a362e2580905fa98146
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r3p3.42sp.org.br>
2024-10-08 09:39:57 -0300

    ex18

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -6,24 +6,33 @@
 /*   By: gcesar-n <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:24:13 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/07 16:38:54 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/08 09:22:31 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);            //teste
-}
-
-int	ft_print_params()
-{
-		
+	write(1, &c, 1);
 }
 
 int	main(int argc, char *argv[])
 {
-	
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
 }
